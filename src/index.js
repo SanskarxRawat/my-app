a21df9471b45a47bf23b6d0d1e3ffb9991ce3f61
SanskarxRawat <rawatavi.211@gmail.com>
2021-04-26 14:51:17 +0530

    init

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import PersistentDrawerLeft from './Home';
 import reportWebVitals from './reportWebVitals';
+const node=document.getElementById('root');
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <PersistentDrawerLeft/>,node);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
